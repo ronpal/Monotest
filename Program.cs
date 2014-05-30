47686ff762d6a5c59ab7cde50d35e2ef45710113
Pål Rønning <pronning@gmail.com>
2014-05-30 07:21:59 +0200

    added program

---
+++ Program.cs
@@ -0,0 +1,15 @@
+
+
+//1: Komprimer H0, finn H1-Hn
+//2: Finn minste Hn 
+//3: Verifiser Hn=H0
+
+public class Program 
+{
+	public static void Main()
+	{
+		Console.Writeline("Hello mono");
+
+	}
+
+}
